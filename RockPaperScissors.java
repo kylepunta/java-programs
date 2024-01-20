b36a1cdd3f93159a78897ee5a42facc806ea0e86
kylepunta <kylepurcell1998@gmail.com>
2024-01-20 19:26:30 +0000

    Created a function called getComputerChoice to randomly generate either Rock, Paper, or Scissors

---
+++ RockPaperScissors.java
@@ -0,0 +1,36 @@
+public class RockPaperScissors 
+{
+    static String getComputerChoice()
+    {
+        String computerChoice = "";
+
+        double randomNumber = Math.floor((Math.random() * 10) + 1);
+
+        while (randomNumber != 1 && randomNumber != 2 && randomNumber != 3)
+            {
+                randomNumber = Math.floor((Math.random() * 10) + 1);
+            }
+        
+        randomNumber = (int)randomNumber;
+
+        if (randomNumber == 1)
+            {
+                computerChoice = "Rock";
+            }
+        else if (randomNumber == 2)
+            {
+                computerChoice = "Paper";
+            }
+        else if (randomNumber == 3)
+            {
+                computerChoice = "Scissors";
+            }
+        
+        return computerChoice;
+    }
+
+    public static void main(String[] args) 
+    {
+        System.out.println(getComputerChoice());
+    }
+}
